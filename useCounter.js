d188642425810bfaa4627b7ee62b7878d657a625
Sergio <mosky66@gmail.com>
2023-04-30 16:10:06 -0300

    Primer commit

---
+++ useCounter.js
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+
+export const useCounter = ( initialValue = 10 ) =>{
+    
+    const [counter, setCounter] = useState(initialValue)
+
+
+    // Ejemplo con un argumento. En este caso, hay que indicar el argumento en la funcion del evento OnClic.
+    const increment = ( value = 1 )=>{
+        setCounter( counter + value );
+        
+    };
+
+    const decrement = () =>{
+        if (counter >= 1) 
+       setCounter( counter -1 ); 
+       
+    };
+
+    const reset = () =>{
+        setCounter( initialValue );
+    };
+
+    return{
+    counter,
+    increment,
+    decrement,
+    reset,
+    }
+
+
+};
